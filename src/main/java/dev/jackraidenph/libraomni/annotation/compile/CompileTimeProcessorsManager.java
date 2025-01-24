e5388b2415eb589242ffe76333d67c9287402907
JackRaidenPH <jraiden.ph@gmail.com>
2025-01-24 18:05:42 +0300

    refactor: rename CompileProcessorManager to CompileTimeProcessorsManager

--- src/main/java/dev/jackraidenph/libraomni/annotation/compile/CompileTimeProcessorsManager.java
+++ src/main/java/dev/jackraidenph/libraomni/annotation/compile/CompileTimeProcessorsManager.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class CompileProcessorManager extends AbstractProcessor {
+public class CompileTimeProcessorsManager extends AbstractProcessor {
 
     List<CompileTimeProcessor> processors = new ArrayList<>();
 
