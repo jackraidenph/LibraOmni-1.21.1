cbf980dea6cbf0608e1684250b18d3f4d074d50a
JackRaidenPH <jraiden.ph@gmail.com>
2025-04-13 02:27:04 +0300

    feat: change ResourceGeneratingProcessor behaviour; Now resources control their path

--- src/main/java/dev/jackraidenph/libraomni/annotation/compilation/ResourceGeneratingProcessor.java
+++ src/main/java/dev/jackraidenph/libraomni/annotation/compilation/ResourceGeneratingProcessor.java
@@ -6,43 +6,15 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.StringJoiner;
-import java.util.regex.Pattern;
 
 abstract class ResourceGeneratingProcessor extends AbstractCompilationProcessor {
 
-    private final String[] rootElements;
-    private static final Pattern ROOT_PATTERN_STRING = Pattern.compile("[A-z_]([\\-\\w]+/)+");
-
     public ResourceGeneratingProcessor(ProcessingEnvironment processingEnvironment, String root) {
         super(processingEnvironment);
-
-        if (!checkRoot(root)) {
-            throw new IllegalArgumentException("Illegal root string, a root should look like: aA/bA0/...cC0/");
-        }
-
-        this.rootElements = root.split("/");
-    }
-
-    private static boolean checkRoot(String toCheck) {
-        return ROOT_PATTERN_STRING.matcher(toCheck).matches();
-    }
-
-    public final String[] getRootElements() {
-        return this.rootElements;
-    }
-
-    public final String getRoot() {
-        StringJoiner stringJoiner = new StringJoiner("/", "", "/");
-        for (String e : this.getRootElements()) {
-            stringJoiner.add(e);
-        }
-        return stringJoiner.toString();
     }
 
     public Filer getFiler() {
@@ -64,14 +36,11 @@ abstract class ResourceGeneratingProcessor extends AbstractCompilationProcessor 
             for (Resource resource : createdFiles) {
 
                 try {
-                    this.createResource(
-                            resource.path(),
-                            new ByteArrayInputStream(resource.bytes())
-                    );
+                    this.createResource(resource);
                 } catch (IOException ioException) {
                     messager.printError(ioException.getLocalizedMessage());
                     messager.printError(
-                            "IOException caught trying to create resource [" + resource.path() + "] at " + this.getRoot()
+                            "IOException caught trying to create resource [" + resource.path() + "]"
                     );
                     continue;
                 }
@@ -86,21 +55,17 @@ abstract class ResourceGeneratingProcessor extends AbstractCompilationProcessor 
         return Set.of();
     }
 
-    public String outputLocation() {
-        return this.getRoot();
-    }
-
-    public final FileObject createResource(String fileName, InputStream contents) throws IOException {
+    public final FileObject createResource(Resource resource) throws IOException {
         Filer filer = this.getFiler();
 
         FileObject fileObject = filer.createResource(
                 StandardLocation.SOURCE_OUTPUT,
                 "",
-                this.outputLocation() + fileName
+                resource.path()
         );
 
         try (OutputStream fileObjectWrite = fileObject.openOutputStream()) {
-            contents.transferTo(fileObjectWrite);
+            fileObjectWrite.write(resource.bytes());
         }
 
         return fileObject;
