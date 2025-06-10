22e1b7b161af07bafca2006575de7f77e51600c9
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-06-10 03:16:32 +0300

    feat: majorly simplify metadata creation

--- src/main/java/dev/jackraidenph/libraomni/compilation/CreateMetadataTask.java
+++ src/main/java/dev/jackraidenph/libraomni/compilation/CreateMetadataTask.java
@@ -1,51 +1,24 @@
 package dev.jackraidenph.libraomni.compilation;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.SetMultimap;
 import dev.jackraidenph.libraomni.annotation.NeedsRuntimeProcessing;
 import dev.jackraidenph.libraomni.annotation.Processor;
-import dev.jackraidenph.libraomni.common.data.ModMetadata;
 import dev.jackraidenph.libraomni.common.data.NativeMetadata;
 import dev.jackraidenph.libraomni.reflect.RuntimeTask.Scope;
 
-import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 class CreateMetadataTask implements CompilationTask {
 
-    private final Set<Element> runtimeElements = new HashSet<>();
-    private final SetMultimap<Scope, Element> runtimeTasksElements = HashMultimap.create();
-    private final Set<String> processableAnnotations = new HashSet<>();
-
     private final NativeMetadata nativeMetadata = new NativeMetadata();
 
-    //UTILITY START
-
-    private String modIdByPackage(ModIdGetter modLocator, Element e) {
-        if (modLocator == null) {
-            return null;
-        }
-        return modLocator.forElement(e);
-    }
-
-    //UTILITY END
-    //METADATA PIPELINE START
-
-    private static boolean isRuntimeAnnotation(Element e) {
-        if (!(e instanceof TypeElement annotationType)) {
-            return false;
-        }
-
-        Retention retention = annotationType.getAnnotation(Retention.class);
+    private static boolean isRuntimeAnnotation(TypeElement annotationTypeElement) {
+        Retention retention = annotationTypeElement.getAnnotation(Retention.class);
         if (retention == null) {
             return false;
         }
@@ -53,82 +26,68 @@ class CreateMetadataTask implements CompilationTask {
         return retention.value().equals(RetentionPolicy.RUNTIME);
     }
 
-    private Set<String> findRuntimeAnnotations(RoundEnvironment roundEnvironment) {
+    //Find custom annotations that need to be processed at runtime
+    private TypeElement[] findRuntimeAnnotations(RoundEnvironment roundEnvironment) {
         return roundEnvironment
+                //Get ALL round elements
                 .getRootElements()
                 .stream()
+                //Get ALL the element's annotations
                 .flatMap(e -> e.getAnnotationMirrors().stream())
+                //Transform them to TypeElement-s
                 .map(am -> (TypeElement) am.getAnnotationType().asElement())
+                //Reject duplicates
+                .distinct()
+                //See if the need to be processed in runtime
                 .filter(e -> e.getAnnotation(NeedsRuntimeProcessing.class) != null)
+                //Check if the annotation actually has RUNTIME retention
                 .filter(CreateMetadataTask::isRuntimeAnnotation)
-                .map(typeElement -> typeElement.getQualifiedName().toString())
-                .collect(Collectors.toSet());
-    }
-
-    private TypeElement[] typesFromStrings(Set<String> names, Elements elements) {
-        return names.stream().map(elements::getTypeElement).toArray(TypeElement[]::new);
+                //Collect to array for later use in RoundEnvironment#getElementsAnnotatedWithAny
+                .toArray(TypeElement[]::new);
     }
 
     @Override
     public Collection<Resource> processRound(ModIdGetter modLocator, RoundEnvironment roundEnv, ProcessingEnvironment processingEnv) {
-        Set<String> runtimeAnnotations = this.findRuntimeAnnotations(roundEnv);
-
-        if (!runtimeAnnotations.isEmpty()) {
-            this.processableAnnotations.addAll(runtimeAnnotations);
-            processingEnv.getMessager().printNote("Found runtime annotations " + this.processableAnnotations);
+        //Find annotations to use for processing runtime data
+        TypeElement[] runtimeAnnotations = findRuntimeAnnotations(roundEnv);
+        if (runtimeAnnotations.length > 0) {
+            processingEnv.getMessager().printNote("Found runtime annotations " + Arrays.toString(runtimeAnnotations));
         }
 
-        this.runtimeElements.addAll(
-                roundEnv.getElementsAnnotatedWithAny(
-                        this.typesFromStrings(this.processableAnnotations, processingEnv.getElementUtils())
-                )
-        );
+        Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWithAny(runtimeAnnotations);
+        for (Element e : annotatedElements) {
+            String modId = modLocator.forElement(e);
+            if (modId == null) {
+                continue;
+            }
 
+            nativeMetadata.getOrCreateModMetadata(modId).getAnnotatedData().addElement(e);
+        }
+
+        //Process user-defined runtime tasks
         for (Element e : roundEnv.getElementsAnnotatedWith(Processor.class)) {
-            Processor annotation = e.getAnnotation(Processor.class);
-            this.runtimeTasksElements.get(annotation.value()).add(e);
+            Processor taskAnnotation = e.getAnnotation(Processor.class);
+            Scope scope = taskAnnotation.value();
+            String name = ((TypeElement) e).getQualifiedName().toString();
+            String modId = modLocator.forElement(e);
+            if (modId == null) {
+                processingEnv.getMessager().printWarning("Got runtime task [" + name + "], but failed to compute the owning mod");
+                continue;
+            }
+
+            nativeMetadata.getOrCreateModMetadata(modId).addRuntimeTask(scope, name);
         }
 
         return Set.of();
     }
 
-    private void addElementsToModData(ModIdGetter modLocator) {
-        for (Element element : this.runtimeElements) {
-            String modId = this.modIdByPackage(modLocator, element);
-            if (modId == null) {
-                continue;
-            }
-
-            nativeMetadata.getOrCreateModMetadata(modId).getAnnotatedData().addElement(element);
-        }
-    }
-
-    private void addTasksToMetadata(ModIdGetter modLocator, Messager messager) {
-        for (Entry<Scope, Collection<Element>> entry : this.runtimeTasksElements.asMap().entrySet()) {
-            Scope scope = entry.getKey();
-            for (Element element : entry.getValue()) {
-                String name = ((TypeElement) element).getQualifiedName().toString();
-                String modId = this.modIdByPackage(modLocator, element);
-                if (modId == null) {
-                    messager.printWarning("Got runtime processor [" + name + "], but failed to compute the owning mod");
-                    continue;
-                }
-
-                ModMetadata modMetadata = nativeMetadata.getOrCreateModMetadata(modId);
-
-                modMetadata.addRuntimeTask(scope, name);
-            }
-        }
-    }
-
     @Override
     public Set<Resource> finish(ModIdGetter modLocator, RoundEnvironment roundEnv, ProcessingEnvironment processingEnv) {
-        this.addElementsToModData(modLocator);
-        this.addTasksToMetadata(modLocator, processingEnv.getMessager());
         return Set.of(
-                Resource.json(nativeMetadata).directory(NativeMetadata.DIRECTORY).name(NativeMetadata.FILE_ROOT).build()
+                Resource.json(nativeMetadata)
+                        .directory(NativeMetadata.DIRECTORY)
+                        .name(NativeMetadata.FILE_ROOT)
+                        .build()
         );
     }
-
-    //METADATA PIPELINE END
 }
