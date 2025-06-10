c527208d98eca7402492f6cab63b0c2609ff4b3a
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-06-10 03:45:14 +0300

    fix: do not rely on exception-catching workaround

--- src/main/java/dev/jackraidenph/libraomni/compilation/ValidateAnnotationsTask.java
+++ src/main/java/dev/jackraidenph/libraomni/compilation/ValidateAnnotationsTask.java
@@ -6,11 +6,10 @@ import dev.jackraidenph.libraomni.compilation.validation.Validator;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.MirroredTypeException;
-import javax.lang.model.type.TypeMirror;
-import javax.lang.model.util.Types;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,11 +26,13 @@ class ValidateAnnotationsTask implements CompilationTask {
                 .collect(Collectors.toSet());
 
         for (TypeElement annotationElement : validatedAnnotations) {
-            Validator validator = this.getValidatorForAnnotation(annotationElement, processingEnv.getTypeUtils());
+            Validator validator = this.getValidatorForAnnotation(annotationElement);
             if (validator == null) {
-                processingEnv.getMessager().printWarning("Failed to get validator from " + annotationElement);
+                processingEnv.getMessager().printError("Failed to get validator from " + annotationElement);
                 continue;
             }
+            processingEnv.getMessager().printNote("Found validator [" + validator.getClass().getSimpleName() + "] for [" + annotationElement.getQualifiedName() + "]");
+
             Set<? extends Element> toValidate = roundEnv.getElementsAnnotatedWith(annotationElement);
 
             for (Element e : toValidate) {
@@ -44,29 +45,49 @@ class ValidateAnnotationsTask implements CompilationTask {
         return Set.of();
     }
 
-    private Validator getValidatorForAnnotation(TypeElement annotationElement, Types types) {
-        Validated validatorAnnotation = annotationElement.getAnnotation(Validated.class);
-        try {
-            validatorAnnotation.value();
-            //This weird hack is the way to get a type mirror after an attempt to access a class
-        } catch (MirroredTypeException mirroredTypeException) {
-            TypeMirror typeMirror = mirroredTypeException.getTypeMirror();
-            Element element = types.asElement(typeMirror);
-            TypeElement typeElement = (TypeElement) element;
+    private AnnotationMirror mirrorByClass(Element e, Class<?> clazz) {
+        for (AnnotationMirror annotationMirror : e.getAnnotationMirrors()) {
+            TypeElement annotationElement = (TypeElement) annotationMirror.getAnnotationType().asElement();
+            if (annotationElement.getQualifiedName().contentEquals(clazz.getName())) {
+                return annotationMirror;
+            }
+        }
 
-            String validatorClassName = typeElement.getQualifiedName().toString();
+        return null;
+    }
+
+    private static Object findAnnotationValue(AnnotationMirror mirror, String valueName) {
+        ExecutableElement executableElement = mirror.getElementValues()
+                .keySet()
+                .stream()
+                .filter(e -> e.getSimpleName().contentEquals(valueName))
+                .findFirst()
+                .orElse(null);
+
+        return mirror.getElementValues().get(executableElement).getValue();
+    }
+
+    private Validator getValidatorForAnnotation(TypeElement annotationElement) {
+        AnnotationMirror validatedMirror = mirrorByClass(annotationElement, Validated.class);
+        if (validatedMirror == null) {
+            return null;
+        }
+
+        try {
+            String validatorClassName = String.valueOf(findAnnotationValue(validatedMirror, "value"));
             Class<? extends Validator> validatorClass = SafeReflectionUtil.forNameSubclass(validatorClassName, Validator.class);
             if (validatorClass == null) {
-                throw new IllegalStateException("No validator class found for name [" + validatorClassName + "]");
+                throw new IllegalStateException("Failed to get Validator for name [" + validatorClassName + "]");
             }
 
             Validator validator = SafeReflectionUtil.tryConstruct(validatorClass);
             if (validator == null) {
-                throw new IllegalStateException("Failed to construct Validator for [" + validatorClassName + "]");
+                throw new IllegalStateException("Failed to construct Validator for [" + validatorClass.getSimpleName() + "]");
             }
-            return validator;
-        }
 
-        return null;
+            return validator;
+        } catch (ClassCastException classCastException) {
+            return null;
+        }
     }
 }
