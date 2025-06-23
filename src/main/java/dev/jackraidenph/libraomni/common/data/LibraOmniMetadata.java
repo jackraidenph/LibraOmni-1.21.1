65e305a71d01734d49c7d3c636a15c84663a1175
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-06-23 03:05:59 +0300

    refactor: remove unnecessary nesting

--- src/main/java/dev/jackraidenph/libraomni/common/data/LibraOmniMetadata.java
+++ src/main/java/dev/jackraidenph/libraomni/common/data/LibraOmniMetadata.java
@@ -12,8 +12,8 @@ public class LibraOmniMetadata {
     @SerializedName("mods")
     private final Map<String, ModMetadata> modMetadata = new HashMap<>();
 
-    public static final String DIRECTORY = "META-INF/" + LibraOmni.MOD_ID + "/";
-    public static final String FILE_ROOT = "libraomni.metadata";
+    public static final String DIRECTORY = "META-INF/";
+    public static final String FILE_ROOT = LibraOmni.MOD_ID + ".metadata";
     public static final String PATH = DIRECTORY + FILE_ROOT + ".json";
 
     public ModMetadata getOrCreateModMetadata(String modId) {
