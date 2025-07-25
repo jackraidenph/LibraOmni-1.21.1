da5a14e4dcb40969fd4ba3101a8fa117947838d4
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-07-25 03:19:25 +0300

    feat: implement AnnotationAccessor and delegate its methods so returned proxy can access original AnnotatedElements

--- src/main/java/dev/jackraidenph/libraomni/data/proxy/AnnotationCachingInvocationHandler.java
+++ src/main/java/dev/jackraidenph/libraomni/data/proxy/AnnotationCachingInvocationHandler.java
@@ -4,16 +4,30 @@ import dev.jackraidenph.libraomni.annotation.Composed;
 import dev.jackraidenph.libraomni.common.SafeReflectionUtil;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class AnnotationCachingInvocationHandler<T> extends ObjectPreservingInvocationHandler<T> {
+public abstract class AnnotationCachingInvocationHandler<T> extends ObjectPreservingInvocationHandler<T> implements AnnotationAccessor<T> {
 
     protected final Map<Class<? extends Annotation>, Annotation> annotationMap = new HashMap<>();
+    private final AnnotationAccessor<T> accessor;
 
     public AnnotationCachingInvocationHandler(T original, AnnotationAccessor<T> accessor) {
         super(original);
+        this.accessor = accessor;
         cacheRecursive(accessor);
+    }
+
+    @Override
+    public T annotatedObject() {
+        return original;
+    }
+
+    @Override
+    public Collection<Annotation> getAllAnnotations() {
+        return accessor.getAllAnnotations();
     }
 
     protected Annotation[] getProxiedRecursiveAnnotations() {
@@ -21,7 +35,7 @@ public abstract class AnnotationCachingInvocationHandler<T> extends ObjectPreser
     }
 
     private void cacheRecursive(AnnotationAccessor<T> accessor) {
-        for (Annotation annotation : accessor.getAnnotations()) {
+        for (Annotation annotation : accessor.getAllAnnotations()) {
             step(annotation);
         }
     }
@@ -48,5 +62,20 @@ public abstract class AnnotationCachingInvocationHandler<T> extends ObjectPreser
                 step(metaProxy);
             }
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) {
+        String name = method.getName();
+        return switch (name) {
+            case "getAllAnnotations" -> getAllAnnotations();
+            case "getAnnotationByClass" -> getAnnotationByClass((Class<? extends Annotation>) args[0]);
+            case "getAnnotationsByClass" -> getAnnotationsByClass((Class<? extends Annotation>) args[0]);
+            case "isAnnotationPresent" -> isAnnotationPresent((Class<? extends Annotation>) args[0]);
+            case "annotatedObject" -> annotatedObject();
+            default -> super.invoke(proxy, method, args);
+        };
+
     }
 }
