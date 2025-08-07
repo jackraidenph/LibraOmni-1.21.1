b86cb15e3a18308ffe9bae9388a7a71b324d3609
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-08-07 06:02:00 +0300

    feat: better tests and adjust for new DFS implementation(the answer is still correct, but different)

--- src/test/java/HashDirectedGraphTest.java
+++ src/test/java/HashDirectedGraphTest.java
@@ -118,9 +118,9 @@ public class HashDirectedGraphTest {
         graph.addNode(3, "3");
         graph.addNode(4, "4");
 
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
         graph.addEdge(0, 3);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 1);
 
         graph.addEdge(1, 4);
         graph.addEdge(2, 4);
@@ -171,8 +171,8 @@ public class HashDirectedGraphTest {
         graph.addEdge(1, 2);
 
         graph.addEdge(0, 3);
-        graph.addEdge(3, 4);
         graph.addEdge(3, 5);
+        graph.addEdge(3, 4);
         graph.addEdge(4, 6);
 
         HashDirectedGraph<String> subgraph = graph.getSubgraph(3);
@@ -209,8 +209,14 @@ public class HashDirectedGraphTest {
         graph.addNode(4, "4");
 
         graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+
         graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+
         graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+
         graph.addEdge(3, 4);
 
         assertFalse(graph.hasCycles());
