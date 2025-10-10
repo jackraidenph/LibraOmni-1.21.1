b71e616822ceb0411a32f7b559ddfe253c721c7f
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-10-10 05:33:42 +0300

    feat: convenient base class for producing compilation tasks

---
+++ src/main/java/dev/jackraidenph/libraomni/compilation/task/SequentialCompilationTask.java
@@ -0,0 +1,51 @@
+package dev.jackraidenph.libraomni.compilation.task;
+
+import dev.jackraidenph.libraomni.annotation.service.Id;
+import dev.jackraidenph.libraomni.compilation.util.InMemoryResource;
+import dev.jackraidenph.libraomni.compilation.util.ModIdGetter;
+
+import javax.annotation.Nullable;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A compilation task that can process elements one-by-one
+ */
+public abstract class SequentialCompilationTask implements CompilationTask {
+
+    public Collection<InMemoryResource> processRound(ModIdGetter modLocator, RoundEnvironment roundEnv, ProcessingEnvironment processingEnv) {
+        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWithAny(supportedAnnotations());
+        return processElements(modLocator, elements, roundEnv, processingEnv);
+    }
+
+    public Collection<InMemoryResource> processElements(ModIdGetter modLocator, Set<? extends Element> elements, RoundEnvironment roundEnv, ProcessingEnvironment processingEnv) {
+        List<InMemoryResource> resources = new ArrayList<>();
+        for (Element e : elements) {
+            if (skipAnnotations() && isAnnotation(e)) {
+                continue;
+            }
+            Id id = e.getAnnotation(Id.class);
+            if (requireIdAnnotation() && id == null) {
+                continue;
+            }
+            resources.addAll(processElement(modLocator.forElement(e), id, e, roundEnv, processingEnv));
+        }
+        return resources;
+    }
+
+    abstract Collection<InMemoryResource> processElement(String modId, @Nullable Id idAnnotation, Element element, RoundEnvironment roundEnv, ProcessingEnvironment processingEnv);
+
+    public boolean requireIdAnnotation() {
+        return false;
+    }
+
+    public boolean skipAnnotations() {
+        return true;
+    }
+
+}
