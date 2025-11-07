1cccb4f40932ca5d3948b91ec540ea8b966731f8
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-11-07 20:37:08 +0300

    refactor: better naming

--- src/main/java/dev/jackraidenph/libraomni/annotation/datagen/GeneratesBlockStateData.java
+++ src/main/java/dev/jackraidenph/libraomni/annotation/datagen/GeneratesBlockStateData.java
@@ -5,8 +5,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
-public @interface BlockStateData {
-    String modelNamespace() default "minecraft";
+public @interface GeneratesBlockStateData {
+    String modelNamespace() default "";
 
     String modelName() default "";
 
