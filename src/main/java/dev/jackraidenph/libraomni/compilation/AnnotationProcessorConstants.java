adbc91cbe2d1efeafff620b777acca75557e7ed0
Ivan Dzyakau <ivandzyakau@gmail.com>
2025-11-09 23:06:16 +0300

    feat: claim new resource generation directories

--- src/main/java/dev/jackraidenph/libraomni/compilation/AnnotationProcessorConstants.java
+++ src/main/java/dev/jackraidenph/libraomni/compilation/AnnotationProcessorConstants.java
@@ -5,8 +5,10 @@ import java.util.*;
 public abstract class AnnotationProcessorConstants {
     public static final Set<String> PROCESSED_RESOURCES = Set.of(
             "data/*/tags/**",
+            "data/*/loot_table/**",
             "assets/*/blockstates/**",
-            "assets/*/models/**"
+            "assets/*/models/**",
+            "assets/*/textures/**"
     );
 
     public static final String NF_MOD_ANNOTATION_CLASS_NAME = "net.neoforged.fml.common.Mod";
