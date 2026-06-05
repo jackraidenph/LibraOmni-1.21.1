9e86db24e0369d88d1fd0ac4c1466e4fa9f6f452
Ivan Dzyakau <ivandzyakau@gmail.com>
2026-06-05 03:09:00 +0300

    feat: implement a utility class to work with images

---
+++ src/main/java/dev/jackraidenph/libraomni/common/ImageHelper.java
@@ -0,0 +1,95 @@
+package dev.jackraidenph.libraomni.common;
+
+import dev.jackraidenph.libraomni.common.ColorUtil.InterpolationMode;
+import dev.jackraidenph.libraomni.compilation.util.JsonMergeHelper.JsonMergeConflictPolicy;
+import dev.jackraidenph.libraomni.compilation.util.ProcessingContext;
+import dev.jackraidenph.libraomni.compilation.util.ResourceIdentifier;
+import dev.jackraidenph.libraomni.compilation.util.ResourceManager;
+
+import javax.annotation.Nullable;
+import javax.annotation.processing.Filer;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+public class ImageHelper {
+
+    public static BufferedImage recolor(BufferedImage originalImage, int[] newPalette, InterpolationMode interpolationMode) {
+        int w = originalImage.getWidth();
+        int h = originalImage.getHeight();
+
+        int[] originalPalette = IntStream.of(originalImage.getRGB(0, 0, w, h, null, 0, w))
+                .filter(argb -> (argb & 0xFF000000) != 0) //Ignore fully transparent
+                .distinct() //Palette of colors
+                .toArray();
+
+        final int originalPaletteLength = originalPalette.length;
+
+        int[] newColors = interpolationMode.equals(InterpolationMode.NONE)
+                ? newPalette
+                : ColorUtil.interpolatePalette(newPalette, originalPaletteLength, interpolationMode);
+
+        if (originalPaletteLength > newColors.length) {
+            throw new IllegalArgumentException("Palette %s has [%d] colors, which is less than the original texture's [%d]".formatted(Arrays.toString(newPalette), newPalette.length, originalPaletteLength));
+        }
+
+        Map<Integer, Integer> colors = ColorUtil.matchOKLAB(originalPalette, newColors);
+
+        return remapColors(originalImage, colors);
+    }
+
+    public static BufferedImage remapColors(BufferedImage originalImage, Map<Integer, Integer> colors) {
+        int w = originalImage.getWidth();
+        int h = originalImage.getHeight();
+
+        BufferedImage newImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+
+        IntStream pixelStream = IntStream.of(originalImage.getRGB(0, 0, w, h, null, 0, w));
+        if (w * h >= (2 << 17)) { //512 x 512 = 262144
+            //noinspection ResultOfMethodCallIgnored
+            pixelStream.parallel();
+        }
+        int[] arr = pixelStream.map(oldRgb -> {
+                    if ((oldRgb & 0xFF000000) == 0) {
+                        return oldRgb;
+                    }
+                    return colors.getOrDefault(oldRgb, oldRgb) | (oldRgb & 0xFF000000);
+                })
+                .toArray();
+
+        newImage.setRGB(0, 0, w, h, arr, 0, w);
+        return newImage;
+    }
+
+    public static void transformAndSavePng(String namespace, String directory, String file, Function<BufferedImage, BufferedImage> transform, ProcessingContext processingContext) {
+        transformAndSavePng(namespace, directory, file, null, transform, processingContext);
+    }
+
+    public static void transformAndSavePng(String namespace, String directory, String file, @Nullable ResourceIdentifier saveTo, Function<BufferedImage, BufferedImage> transform, ProcessingContext processingContext) {
+        ResourceManager resourceManager = processingContext.resourceManager();
+        Filer filer = processingContext.processingEnvironment().getFiler();
+        ResourceIdentifier textureLocation = ResourceIdentifier.pngAsset(namespace, directory, file);
+        Optional<Path> existingTexture = textureLocation.atLocation(filer);
+        if (existingTexture.isEmpty()) {
+            existingTexture = textureLocation.atLocation(processingContext.config().getResourceSetDirs());
+        }
+        if (existingTexture.isEmpty()) {
+            throw new IllegalStateException("Failed to find [%s]".formatted(textureLocation));
+        }
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            BufferedImage image = ImageIO.read(existingTexture.get().toFile());
+            BufferedImage newImage = transform.apply(image);
+            ImageIO.write(newImage, "png", outputStream);
+            byte[] bytes = outputStream.toByteArray();
+            resourceManager.save(saveTo == null ? textureLocation : saveTo, bytes, JsonMergeConflictPolicy.OVERWRITE);
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
+    }
+}
