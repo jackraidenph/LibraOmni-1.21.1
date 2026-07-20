c599a3b4d9b7b3bc4d72897ee750879a892a3c3e
Ivan Dzyakau <ivandzyakau@gmail.com>
2026-07-20 04:06:04 +0300

    refactor: wrong naming

--- src/main/java/dev/jackraidenph/libraomni/annotation/meta/InterceptorFor.java
+++ src/main/java/dev/jackraidenph/libraomni/annotation/meta/InterceptorFor.java
@@ -1,18 +1,16 @@
 package dev.jackraidenph.libraomni.annotation.meta;
 
-import dev.jackraidenph.libraomni.annotation.meta.InterceptorFor.InvokerForContainer;
-
 import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Repeatable(InvokerForContainer.class)
+@Repeatable(InterceptorFor.Container.class)
 public @interface InterceptorFor {
     String value();
 
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
-    @interface InvokerForContainer {
+    @interface Container {
         InterceptorFor[] value();
     }
 }
