3ec0c20064452181cf4952dd26fe716c243dfdd0
Ivan Dzyakau <ivandzyakau@gmail.com>
2026-07-21 04:11:14 +0300

    feat(!): generally improve annotation semantics and fix tasks accordingly

--- src/main/java/dev/jackraidenph/libraomni/annotation/datagen/ArbitraryBlockModelData.java
+++ src/main/java/dev/jackraidenph/libraomni/annotation/datagen/ArbitraryBlockModelData.java
@@ -1,7 +1,6 @@
 package dev.jackraidenph.libraomni.annotation.datagen;
 
 
-import dev.jackraidenph.libraomni.annotation.datagen.ArbitraryBlockModelData.Container;
 import dev.jackraidenph.libraomni.annotation.info.GeneratesFiles;
 import dev.jackraidenph.libraomni.annotation.info.Internal;
 import dev.jackraidenph.libraomni.annotation.value.StringPair;
