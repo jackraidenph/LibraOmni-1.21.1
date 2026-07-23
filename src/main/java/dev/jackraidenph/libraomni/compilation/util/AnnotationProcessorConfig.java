bbf6de75053f600315b199f5e5caa06946b4588b
Ivan Dzyakau <ivandzyakau@gmail.com>
2026-07-23 22:35:33 +0300

    feat: instead of throwing, choose first encountered policy

--- src/main/java/dev/jackraidenph/libraomni/compilation/util/AnnotationProcessorConfig.java
+++ src/main/java/dev/jackraidenph/libraomni/compilation/util/AnnotationProcessorConfig.java
@@ -122,21 +122,14 @@ public final class AnnotationProcessorConfig {
             throw new RuntimeException("Not a path [%s]".formatted(resourcePath));
         }
 
-        Set<String> matches = new HashSet<>();
-        JsonMergeConflictPolicy policy = null;
         for (Entry<PathMatcher, JsonMergeConflictPolicy> e : conf.entrySet()) {
             PathMatcher globMatcher = e.getKey();
             if (globMatcher.matches(path)) {
-                matches.add(e.getValue().name());
-                policy = e.getValue();
+                return e.getValue();
             }
         }
 
-        if (matches.size() > 1) {
-            throw new IllegalStateException("Multiple pattern matches for [" + resourcePath + "]: " + matches);
-        }
-
-        return policy;
+        return null;
     }
 
 }
