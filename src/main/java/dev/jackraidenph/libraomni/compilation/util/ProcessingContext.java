797cd5dd3fa84e3803c485c6c7453f9fef7f69fc
Ivan Dzyakau <ivandzyakau@gmail.com>
2026-07-23 21:39:36 +0300

    feat: redo ProcessingContext from record into a data class with setters to resolve potential circular dependencies

--- src/main/java/dev/jackraidenph/libraomni/compilation/util/ProcessingContext.java
+++ src/main/java/dev/jackraidenph/libraomni/compilation/util/ProcessingContext.java
@@ -1,14 +1,75 @@
 package dev.jackraidenph.libraomni.compilation.util;
 
+import javax.annotation.Nullable;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 
-public record ProcessingContext(
-        ModIdGetter modIdGetter,
-        ResourceManager resourceManager,
-        AnnotationProcessorConfig config,
-        RoundEnvironment roundEnvironment,
-        ProcessingEnvironment processingEnvironment,
-        int round
-) {
+public class ProcessingContext {
+
+    private ModIdGetter modIdGetter;
+    private ResourceManager resourceManager;
+    private AnnotationProcessorConfig config;
+    private RoundEnvironment roundEnvironment;
+    private ProcessingEnvironment processingEnvironment;
+    private final int round;
+
+    public ProcessingContext(
+            @Nullable ModIdGetter modIdGetter,
+            @Nullable ResourceManager resourceManager,
+            @Nullable AnnotationProcessorConfig config,
+            @Nullable RoundEnvironment roundEnvironment,
+            @Nullable ProcessingEnvironment processingEnvironment,
+            int round
+    ) {
+        this.modIdGetter = modIdGetter;
+        this.resourceManager = resourceManager;
+        this.config = config;
+        this.roundEnvironment = roundEnvironment;
+        this.processingEnvironment = processingEnvironment;
+        this.round = round;
+    }
+
+    public ModIdGetter modIdGetter() {
+        return modIdGetter;
+    }
+
+    public void setModIdGetter(ModIdGetter modIdGetter) {
+        this.modIdGetter = modIdGetter;
+    }
+
+    public ResourceManager resourceManager() {
+        return resourceManager;
+    }
+
+    public void setResourceManager(ResourceManager resourceManager) {
+        this.resourceManager = resourceManager;
+    }
+
+    public AnnotationProcessorConfig config() {
+        return config;
+    }
+
+    public void setConfig(AnnotationProcessorConfig config) {
+        this.config = config;
+    }
+
+    public RoundEnvironment roundEnvironment() {
+        return roundEnvironment;
+    }
+
+    public void setRoundEnvironment(RoundEnvironment roundEnvironment) {
+        this.roundEnvironment = roundEnvironment;
+    }
+
+    public ProcessingEnvironment processingEnvironment() {
+        return processingEnvironment;
+    }
+
+    public void setProcessingEnvironment(ProcessingEnvironment processingEnvironment) {
+        this.processingEnvironment = processingEnvironment;
+    }
+
+    public int round() {
+        return round;
+    }
 }
