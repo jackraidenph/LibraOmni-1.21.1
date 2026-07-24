9c61bb7ff9afb671159fd3467e0349ed297ef697
Ivan Dzyakau <ivandzyakau@gmail.com>
2026-07-24 03:48:34 +0300

    feat: timing utilities

--- src/main/java/dev/jackraidenph/libraomni/compilation/task/CompilationTaskProcessor.java
+++ src/main/java/dev/jackraidenph/libraomni/compilation/task/CompilationTaskProcessor.java
@@ -1,6 +1,7 @@
 package dev.jackraidenph.libraomni.compilation.task;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.base.Supplier;
 import dev.jackraidenph.libraomni.annotation.meta.ModPackage;
 import dev.jackraidenph.libraomni.compilation.task.cache.ProcessingCache;
 import dev.jackraidenph.libraomni.compilation.task.cache.RoundCache;
@@ -13,11 +14,13 @@ import dev.jackraidenph.libraomni.compilation.AnnotationProcessorConstants;
 import dev.jackraidenph.libraomni.experimental.BlackMagicBootstrap;
 import dev.jackraidenph.libraomni.experimental.BlackMagicUtil;
 
+import javax.annotation.Nullable;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_21)
 public final class CompilationTaskProcessor extends AbstractProcessor {
@@ -29,8 +32,7 @@ public final class CompilationTaskProcessor extends AbstractProcessor {
     private final AnnotationProcessorConfig config = new AnnotationProcessorConfig();
     private final ProcessingCache cache = new ProcessingCache();
 
-    private final Stopwatch stopwatch = Stopwatch.createUnstarted();
-    private final Stopwatch stopwatchFull = Stopwatch.createUnstarted();
+    private final Stopwatch processingStopwatch = Stopwatch.createUnstarted();
 
     private int round = 0;
 
@@ -62,12 +64,11 @@ public final class CompilationTaskProcessor extends AbstractProcessor {
         return MOD_ID_GETTER;
     }
 
-    private long elapsedTotal = 0;
-
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        stopwatchFull.reset();
-        stopwatchFull.start();
+        if (!processingStopwatch.isRunning()) {
+            processingStopwatch.start();
+        }
 
         discoverMods(roundEnvironment);
 
@@ -80,14 +81,6 @@ public final class CompilationTaskProcessor extends AbstractProcessor {
 
         Messager messager = this.processingEnv.getMessager();
 
-        if (round == 0) {
-            stopwatch.reset();
-            stopwatch.start();
-            BlackMagicUtil.compileAndLoad(context);
-            long elapsedCompile = stopwatch.elapsed().getNano();
-            messager.printNote("Compiling took %.4f seconds".formatted(elapsedCompile / 1_000_000_000.));
-        }
-
         boolean processignOver = roundEnvironment.processingOver();
 
         if (!processignOver) {
@@ -96,12 +89,8 @@ public final class CompilationTaskProcessor extends AbstractProcessor {
             messager.printNote("Finishing");
         }
 
-        stopwatch.reset();
-        stopwatch.start();
         RoundCache oldCache = RoundCache.readFromTempDir(round);
         RoundCache newCache = cache.cacheRoundElements(context, tasks);
-        long elapsedCache = stopwatch.elapsed().getNano();
-        messager.printNote("Reading and calculating cache took %.4f seconds".formatted(elapsedCache / 1_000_000_000.));
 
         for (CompilationTask compilationTask : this.tasks) {
 
@@ -112,43 +101,56 @@ public final class CompilationTaskProcessor extends AbstractProcessor {
             dirtyTasks.add(compilationTask.className());
 
             String simpleTaskName = compilationTask.getClass().getSimpleName();
-
             String op = processignOver ? "Finishing" : "Processing";
 
             if (!tryEnableBlackMagic(compilationTask, context, op)) {
                 continue;
             }
 
-            stopwatch.reset();
-            stopwatch.start();
-
-            boolean executed;
-            try {
-                executed = compilationTask.processStage(context);
-            } catch (Exception e) {
-                RoundCache.removeFromTempDir(round);
-                printStackTrace(e);
-                throw new RuntimeException("Exception thrown while processing [%s]".formatted(compilationTask.getClass().getSimpleName()), e);
-            }
-
-            long elapsed = stopwatch.elapsed().getNano();
-            if (executed) {
-                messager.printNote("%s [%s] took %.4f seconds".formatted(op, simpleTaskName, elapsed / 1_000_000_000.));
-            }
+            timed(() -> {
+                        try {
+                            return compilationTask.processStage(context);
+                        } catch (Exception e) {
+                            RoundCache.removeFromTempDir(round);
+                            printStackTrace(e);
+                            throw new RuntimeException("Exception thrown while processing [%s]".formatted(compilationTask.getClass().getSimpleName()), e);
+                        }
+                    },
+                    "%s [%s]".formatted(op, simpleTaskName),
+                    messager
+            );
         }
 
         newCache.setBuilt(true);
         newCache.saveToTempDir(round);
-        elapsedTotal += stopwatchFull.elapsed().getNano();
 
         if (processignOver) {
-            //Restore original Log4J config
             BlackMagicUtil.restoreLog4j();
-            messager.printNote("LibraOmni processor finished, took %.4f seconds".formatted(elapsedTotal / 1_000_000_000.));
+            double elapsedSeconds = processingStopwatch.elapsed().getNano() / 1_000_000_000.;
+            messager.printNote("LibraOmni processor finished, took %.4f seconds".formatted(elapsedSeconds));
+            processingStopwatch.stop();
         }
 
         this.round++;
         return false;
+    }
+
+    private void timed(Runnable action, String actionComment, @Nullable Messager messager) {
+        timed(() -> {
+            action.run();
+            return true;
+        }, actionComment, messager);
+    }
+
+    private void timed(Supplier<Boolean> action, String actionComment, @Nullable Messager messager) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        boolean result = action.get();
+        long nanos = stopwatch.elapsed(TimeUnit.NANOSECONDS);
+        double seconds = nanos / 1_000_000_000.;
+        if (result && messager != null) {
+            messager.printNote("%s | %.4f seconds".formatted(actionComment, seconds));
+        }
+        stopwatch.stop();
     }
 
     private boolean tryEnableBlackMagic(CompilationTask task, ProcessingContext context, String op) {
@@ -163,12 +165,8 @@ public final class CompilationTaskProcessor extends AbstractProcessor {
             return false;
         }
 
-        if (!BlackMagicBootstrap.isBlackMagicActive()) {
-            stopwatch.reset();
-            stopwatch.start();
-            BlackMagicBootstrap.bootstrapBlackMagic(modIdGetter, context);
-            long elapsedBootstrap = stopwatch.elapsed().getNano();
-            messager.printNote("Bootstrapping Black Magic took %.4f seconds".formatted(elapsedBootstrap / 1_000_000_000.));
+        if (task.requiresBlackMagicEnabled() && !BlackMagicBootstrap.isBlackMagicActive()) {
+            timed(() -> BlackMagicBootstrap.bootstrapBlackMagic(modIdGetter, context), "Bootstrapping Black Magic", messager);
         }
 
         return true;
